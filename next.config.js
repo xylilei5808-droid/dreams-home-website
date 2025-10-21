509923d150a04997de380eb5112231da883c1a3b
dahuangfengxxx <lilei5808@gmail.com>
2025-10-22 00:39:20 +0800

    Fix build errors for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,43 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
-  images: {
-    domains: [
-      'www.notion.so',
-      's3.us-west-2.amazonaws.com',
-      'prod-files-secure.s3.us-west-2.amazonaws.com',
-      'images.unsplash.com'
-    ],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.notion.so',
-      }
-    ]
-  },
-  env: {
-    NOTION_TOKEN: process.env.NOTION_TOKEN,
-    NOTION_PAGE_ID: process.env.NOTION_PAGE_ID,
-    NOTION_ROOMS_DB_ID: process.env.NOTION_ROOMS_DB_ID,
-    NOTION_JOURNAL_DB_ID: process.env.NOTION_JOURNAL_DB_ID,
-    NOTION_PLANS_DB_ID: process.env.NOTION_PLANS_DB_ID,
-    NOTION_STORIES_DB_ID: process.env.NOTION_STORIES_DB_ID,
-    NOTION_EXPERIENCES_DB_ID: process.env.NOTION_EXPERIENCES_DB_ID,
-    NOTION_CUISINES_DB_ID: process.env.NOTION_CUISINES_DB_ID,
-    NOTION_DISHES_DB_ID: process.env.NOTION_DISHES_DB_ID,
-    NOTION_CHEFS_DB_ID: process.env.NOTION_CHEFS_DB_ID,
-  },
-  // 修复构建问题
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: [
+      'www.notion.so',
+      's3.us-west-2.amazonaws.com',
+      'prod-files-secure.s3.us-west-2.amazonaws.com'
+    ]
   }
 }
 
